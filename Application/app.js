91232e245db1bebe5ab53f78d5e6bd56e111b0c5
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-05-11 19:34:51 +0200

    UPDATE EVERYTHING
    everything.

--- Application/app.js
+++ Application/app.js
@@ -1,11 +1,57 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const logger = require('./src/util/utils').logger;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = process.env.PORT || 3000;
 
+
+app.use('*', (req, res, next) => {
+  const method = req.method;
+  logger.trace(`Methode ${method} is aangeroepen`);
+  next();
+});
+
+// References to the routes;
+const userRoutes = require('./src/routes/user.routes');
+app.use('/api/user', userRoutes);
+
+// UC-102 Opvragen van systeeminformatie
+app.get('/api/info', (req, res) => {
+  logger.info('Get server information');
+  res.status(201).json({
+    status: 201,
+    message: 'Server-info endpoint',
+    data: {
+      studentName: 'Ivan van DIjk',
+      studentNumber: 2196154,
+      description: 'This is my express server for the class Porgamming-4'
+    }
+  });
+});
+
+// invalid URL, catch all solution
+app.use('*', (req, res) => {
+  logger.warn('Invalid endpoint called: ', req.path);
+  res.status(404).json({
+    status: 404,
+    message: 'Endpoint not found',
+    data: {}
+  });
+});
+
+// Express error handler
+app.use((err, req, res, next) => {
+  logger.error(err.code, err.message);
+  res.status(err.code).json({
+    statusCode: err.code,
+    message: err.message,
+    data: {}
+  });
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  logger.info(`Server started on port ${port}`);
+});
+
+module.exports = app;
