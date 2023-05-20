c0c4be5d82645a204716b3762c9fef91df21ddfe
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-05-20 23:36:01 +0200

    Update meal.controller.js

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -72,13 +72,14 @@ const mealController = {
         logger.debug(meal);
         
         // Establish a database connection
-        dbconnection.getConnection((err, connection) => {
+        pool.getConnection((err, conn) => {
             if (err) {
-            const error = {
-                status: 500,
-                message: 'Failed to establish database connection.',
-            };
-            return next(error);
+            // Handle connection error
+            logger.error(err.code, err.syscall, err.address, err.port);
+            return next({
+                code: 500,
+                message: err.code
+            });
             }
         
             // Add new meal to the database
@@ -101,7 +102,7 @@ const mealController = {
             meal.isToTakeHome,
             ];
         
-            connection.query(insertQuery, insertParams, (error, results, fields) => {
+            conn.query(insertQuery, insertParams, (error, results, fields) => {
             
                 if (error) {
                     logger.debug(error);
@@ -114,9 +115,9 @@ const mealController = {
             
                 // Retrieve the inserted meal from the database
                 const selectQuery = 'SELECT * FROM meal ORDER BY id DESC LIMIT 1;';
-                connection.query(selectQuery, (error, results, fields) => {
-                    connection.release();
-                    
+                conn.query(selectQuery, (error, results, fields) => {
+                    conn.release();
+
                     if (error) {
                     const newError = {
                         status: 500,
@@ -142,41 +143,235 @@ const mealController = {
                 });
             });
         });
-    },   
+    },  
+    
+    
+    // UC-302 Wijzingen van maaltijdgegevens
+	updateMealById: (req, res, next) => {
+        const mealId = req.params.mealId;
+        const newMealInfo = req.body;
+        const price = parseFloat(newMealInfo.price);
+        const updateAllergenes = req.body.allergenes.join();
+      
+        pool.getConnection((err, conn) => {
+          if (err) {
+            const error = {
+              code: 500,
+              message: err.code
+            };
+            return next(error);
+          }
+      
+          const sqlUpdateQuery =
+            'UPDATE meal SET name = ?, description = ?, isActive = ?, isVega = ?, isVegan = ?, isToTakeHome = ?, dateTime = STR_TO_DATE(?,"%Y-%m-%dT%H:%i:%s.%fZ"), imageUrl = ?, allergenes = ?, maxAmountOfParticipants = ?, price = ? WHERE id = ?';
+      
+          conn.query(
+            sqlUpdateQuery,
+            [
+              newMealInfo.name,
+              newMealInfo.description,
+              newMealInfo.isActive,
+              newMealInfo.isVega,
+              newMealInfo.isVegan,
+              newMealInfo.isToTakeHome,
+              newMealInfo.dateTime,
+              newMealInfo.imageUrl,
+              updateAllergenes,
+              newMealInfo.maxAmountOfParticipants,
+              price,
+              mealId
+            ],
+            (error, results, fields) => {
+                conn.release();
+        
+                if (error) {
+                    const newError = {
+                    code: 404,
+                    message: `Meal with ID ${mealId} not found`
+                    };
+                    return next(newError);
+                }
+        
+                if (results.affectedRows > 0) {
+                    dbconnection.query(
+                    'SELECT * FROM meal WHERE id = ?',
+                    [mealId],
+                    (error, results, fields) => {
+                        if (error) {
+                        const newError = {
+                            code: 500,
+                            message: error.message
+                        };
+                        return next(newError);
+                        }
+        
+                        results[0].price = price;
+                        results[0].isActive = newMealInfo.isActive ? true : false;
+                        results[0].isVega = newMealInfo.isVega ? true : false;
+                        results[0].isVegan = newMealInfo.isVegan ? true : false;
+                        results[0].isToTakeHome = newMealInfo.isToTakeHome ? true : false;
+        
+                        res.status(200).json({
+                        code: 200,
+                        message: 'Update Meal by ID',
+                        result: results[0]
+                        });
+                    }
+                    );
+                } else {
+                    const error = {
+                    code: 404,
+                    message: `Meal with ID ${mealId} not found`
+                    };
+                    return next(error);
+                }
+            }
+            );
+        });
+    },
+        
+
     
     // UC-303 Opvragen van alle maaltijden
     getAllMeals: (req, res, next) => {
         logger.info('getAllMeals called');
+
+        // Establish a database connection
+        pool.getConnection((err, conn) => {
+            if (err) {
+            // Handle connection error
+            logger.error(err.code, err.syscall, err.address, err.port);
+            return next({
+                code: 500,
+                message: err.code
+            });
+            }
+
+            // Define the query to retrieve all meals
+            const query = 'SELECT * FROM meal';
+
+            // Execute the query
+            conn.query(query, (error, result, fields) => {
+                conn.release();
+
+                if (error) {
+                    // Handle query execution error
+                    const newError = {
+                    status: 500,
+                    message: 'Failed to retrieve meals from the database.',
+                    };
+                    return next(newError);
+                }
+
+                // Return the retrieved meals as a response
+                res.status(200).json({
+                    status: 200,
+                    result: result,
+                });
+            });
+        });
+    },
+
+    // UC-304 Opvragen van maaltijd bij ID
+	getMealById: (req, res, next) => {
+        const mealId = req.params.mealId;
+        const sqlStatement = 'SELECT * FROM meal WHERE id = ?';
       
-        dbconnection.getConnection((error, connection) => {
-          if (error) {
-            const newError = {
-              status: 500,
-              message: 'Failed to establish database connection.',
-            };
-            return next(newError);
-          }
+        // Get a connection from the pool
+        pool.getConnection((err, conn) => {
+            if (err) {
+            // Handle connection error
+            logger.error(err.code, err.syscall, err.address, err.port);
+            return next({
+                code: 500,
+                message: err.code
+            });
+            }
+        
+            // Execute the SQL query
+            connection.query(sqlStatement, [mealId], (err, results, fields) => {
+                connection.release();
+            
+                if (err) {
+                    logger.error(err.message);
+                    return next({
+                    code: 409,
+                    message: err.message
+                    });
+                }
+            
+                if (results.length < 1) {
+                    const error = {
+                    code: 404,
+                    message: `Meal with ID: ${mealId} not found!`
+                    };
+                    return next(error);
+                }
+            
+                // Modify result properties to boolean values
+                results[0].isActive = !!results[0].isActive;
+                results[0].isVega = !!results[0].isVega;
+                results[0].isVegan = !!results[0].isVegan;
+                results[0].isToTakeHome = !!results[0].isToTakeHome;
+            
+                // Send the result as a response
+                res.status(200).json({
+                    code: 200,
+                    message: 'Get Meal by ID',
+                    data: results[0]
+                });
+            });
+        });
+    },
+
+    deleteMealById: (req, res, next) => {
+        const mealId = req.params.mealId;
       
-          const query = 'SELECT * FROM meal';
-      
-          connection.query(query, (error, result, fields) => {
-            connection.release();
-      
-            if (error) {
-              const newError = {
-                status: 500,
-                message: 'Failed to retrieve meals from the database.',
-              };
-              return next(newError);
+        pool.getConnection((err, conn) => {
+            if (err) {
+            // Handle connection error
+            logger.error(err.code, err.syscall, err.address, err.port);
+            return next({
+                code: 500,
+                message: err.code
+            });
             }
       
-            logger.debug('result=', result.length);
-            res.status(200).json({
-              status: 200,
-              result: result,
-            });
-          });
+          conn.query(
+                'DELETE FROM meal WHERE id = ?',
+                [mealId],
+                (error, results) => {
+                    conn.release();
+            
+                    if (error) {
+                        // Handle query error
+                        const error = {
+                        status: 500,
+                        message: 'Failed to delete meal.',
+                        };
+                        next(error);
+                        return;
+                    }
+            
+                    if (results.affectedRows === 0) {
+                        // No meal found with the provided mealId
+                        const error = {
+                        status: 404,
+                        message: `Meal with ID ${mealId} not found.`,
+                        };
+                        next(error);
+                        return;
+                    }
+            
+                    res.status(200).json({
+                        status: 200,
+                        message: `Meal with ID ${mealId} successfully deleted.`,
+                    });
+                }
+            );
         });
-      },
+    },
+      
+      
       
 }
