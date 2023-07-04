57b0db2fa6db74d3ccccb9d4799dc1fd94828bac
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-07-04 19:14:01 +0200

    Update users.test.js

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -455,212 +455,196 @@ describe('User Tests', () => {
   });
   // ----- UC-204 Opvragen van usergegevens bij ID -----
 
-//   // UC-205 Updaten van usergegevens
-//   describe('UC-205 Updaten van usergegevens', () => {
-//     let token = '';
+  // UC-205 Updaten van usergegevens
+  describe('UC-205 Updaten van usergegevens', () => {
+    let token = jwt.sign({ userId: 99 }, jwtSecretKey);
 
-//     beforeEach((done) => {
-//       logger.debug('beforeEach called');
-//       pool.getConnection(function (err, conn) {
-//         if (err) throw err;
-//         conn.query(
-//           'ALTER TABLE `user` AUTO_INCREMENT = 1;',
-//           function (error, result, fields) {
-//             conn.query(
-//               CLEAR_USERS_TABLE,
-//               function (error, results, fields) {
-//                 // Create a user for testing
-//                 const userData = {
-//                   firstName: 'John',
-//                   lastName: 'Doe',
-//                   emailAdress: 'johndoe@example.com',
-//                   password: 'Password1!',
-//                   phoneNumber: '123456789',
-//                   street: '123 Street',
-//                   city: 'City'
-//                 };
+    beforeEach((done) => {
+        logger.debug('beforeEach called');
+        pool.getConnection(function (err, conn) {
+          if (err) throw err;
+          conn.query(
+            'ALTER TABLE `user` AUTO_INCREMENT = 1;',
+            function (error, result, fields) {
+              conn.query(
+                CLEAR_USERS_TABLE + INSERT_USER + INSERT_USER2,
+                function (error, results, fields) {
+                  conn.release();
+                  if (error) throw error;
+                  logger.debug('beforeEach done');
+                  done();
+                }
+              );
+            }
+          );
+        });
+      });
 
-//                 userController.createUser(userData, (error, result) => {
-//                   // Generate a token for the user
-//                   token = jwt.sign({ userId: result.insertId }, jwtSecretKey);
+    it('TC-205-1 Verplicht veld "emailAdress" ontbreekt', (done) => {
+      const userId = 99;
+      const updatedUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'Password2!',
+        phoneNumber: '987654321',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//                   conn.release();
-//                   if (error) throw error;
-//                   logger.debug('beforeEach done');
-//                   done();
-//                 });
-//               }
-//             );
-//           }
-//         );
-//       });
-//     });
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message } = res.body;
+          // Verify that the response status and error message are correct
+          expect(status).to.equal(400);
+          expect(message).to.equal('Missing field: emailAdress');
+          done();
+        });
+    });
 
-//     it('TC-205-1 Verplicht veld "emailAdress" ontbreekt', (done) => {
-//       const userId = 1;
-//       const updatedUserData = {
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         password: 'Password2!',
-//         phoneNumber: '987654321',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
+    it('TC-205-2 De gebruiker is niet de eigenaar van de data', (done) => {
+      const userId = 99;
+      const updatedUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        emailAdress: 'johndoe@example.com',
+        password: 'Password2!',
+        phoneNumber: '987654321',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message } = res.body;
-//           // Verify that the response status and error message are correct
-//           expect(status).to.equal(400);
-//           expect(message).to.equal('Email address is required');
-//           done();
-//         });
-//     });
+      // Generate a token for a different user
+      const differentUserToken = jwt.sign({ userId: 999 }, jwtSecretKey);
 
-//     it('TC-205-2 De gebruiker is niet de eigenaar van de data', (done) => {
-//       const userId = 1;
-//       const updatedUserData = {
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         emailAdress: 'johndoe@example.com',
-//         password: 'Password2!',
-//         phoneNumber: '987654321',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .set('Authorization', `Bearer ${differentUserToken}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message } = res.body;
+          // Verify that the response status and error message are correct
+          expect(status).to.equal(403);
+          expect(message).to.equal('Forbidden: You are not the owner of this user');
+          done();
+        });
+    });
 
-//       // Generate a token for a different user
-//       const differentUserToken = jwt.sign({ userId: 999 }, jwtSecretKey);
+    it('TC-205-3 Niet-valide telefoonnummer', (done) => {
+      const userId = 1;
+      const updatedUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        emailAdress: 'johndoe@example.com',
+        password: 'Password2!',
+        phoneNumber: '123',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .set('Authorization', `Bearer ${differentUserToken}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message } = res.body;
-//           // Verify that the response status and error message are correct
-//           expect(status).to.equal(403);
-//           expect(message).to.equal('Forbidden: You are not the owner of this user');
-//           done();
-//         });
-//     });
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message } = res.body;
+          // Verify that the response status and error message are correct
+          expect(status).to.equal(400);
+          expect(message).to.equal('Phone number is not valid');
+          done();
+        });
+    });
 
-//     it('TC-205-3 Niet-valide telefoonnummer', (done) => {
-//       const userId = 1;
-//       const updatedUserData = {
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         emailAdress: 'johndoe@example.com',
-//         password: 'Password2!',
-//         phoneNumber: '123',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
+    it('TC-205-4 Gebruiker bestaat niet', (done) => {
+      const userId = 999;
+      const updatedUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        emailAdress: 'johndoe@example.com',
+        password: 'Password2!',
+        phoneNumber: '987654321',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message } = res.body;
-//           // Verify that the response status and error message are correct
-//           expect(status).to.equal(400);
-//           expect(message).to.equal('Phone number is not valid');
-//           done();
-//         });
-//     });
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message } = res.body;
+          // Verify that the response status and error message are correct
+          expect(status).to.equal(404);
+          expect(message).to.equal('User not found');
+          done();
+        });
+    });
 
-//     it('TC-205-4 Gebruiker bestaat niet', (done) => {
-//       const userId = 999;
-//       const updatedUserData = {
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         emailAdress: 'johndoe@example.com',
-//         password: 'Password2!',
-//         phoneNumber: '987654321',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
+    it('TC-205-5 Niet ingelogd', (done) => {
+      const userId = 1;
+      const updatedUserData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        emailAdress: 'johndoe@example.com',
+        password: 'Password2!',
+        phoneNumber: '987654321',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message } = res.body;
-//           // Verify that the response status and error message are correct
-//           expect(status).to.equal(404);
-//           expect(message).to.equal('User not found');
-//           done();
-//         });
-//     });
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message } = res.body;
+          // Verify that the response status and error message are correct
+          expect(status).to.equal(401);
+          expect(message).to.equal('Unauthorized: Missing or invalid token');
+          done();
+        });
+    });
 
-//     it('TC-205-5 Niet ingelogd', (done) => {
-//       const userId = 1;
-//       const updatedUserData = {
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         emailAdress: 'johndoe@example.com',
-//         password: 'Password2!',
-//         phoneNumber: '987654321',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
+    it('TC-205-6 Gebruiker succesvol gewijzigd', (done) => {
+      const userId = 1;
+      const updatedUserData = {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        emailAdress: 'janedoe@example.com',
+        password: 'Password2!',
+        phoneNumber: '987654321',
+        street: '456 Street',
+        city: 'Town'
+      };
 
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message } = res.body;
-//           // Verify that the response status and error message are correct
-//           expect(status).to.equal(401);
-//           expect(message).to.equal('Unauthorized: Missing or invalid token');
-//           done();
-//         });
-//     });
-
-//     it('TC-205-6 Gebruiker succesvol gewijzigd', (done) => {
-//       const userId = 1;
-//       const updatedUserData = {
-//         firstName: 'Jane',
-//         lastName: 'Doe',
-//         emailAdress: 'janedoe@example.com',
-//         password: 'Password2!',
-//         phoneNumber: '987654321',
-//         street: '456 Street',
-//         city: 'Town'
-//       };
-
-//       chai.request(server)
-//         .put(`/api/user/${userId}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .send(updatedUserData)
-//         .end((err, res) => {
-//           res.should.be.an('object');
-//           let { status, message, data } = res.body;
-//           // Verify that the response status, message, and data are correct
-//           expect(status).to.equal(200);
-//           expect(message).to.equal('User updated successfully');
-//           expect(data).to.be.an('object');
-//           expect(data.firstName).to.equal('Jane');
-//           expect(data.lastName).to.equal('Doe');
-//           expect(data.emailAdress).to.equal('janedoe@example.com');
-//           expect(data.phoneNumber).to.equal('987654321');
-//           expect(data.street).to.equal('456 Street');
-//           expect(data.city).to.equal('Town');
-//           done();
-//         });
-//     });
-//   });
-//   // ----- UC-205 Updaten van usergegevens -----
+      chai.request(server)
+        .put(`/api/user/${userId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedUserData)
+        .end((err, res) => {
+          res.should.be.an('object');
+          let { status, message, data } = res.body;
+          // Verify that the response status, message, and data are correct
+          expect(status).to.equal(200);
+          expect(message).to.equal('User updated successfully');
+          expect(data).to.be.an('object');
+          expect(data.firstName).to.equal('Jane');
+          expect(data.lastName).to.equal('Doe');
+          expect(data.emailAdress).to.equal('janedoe@example.com');
+          expect(data.phoneNumber).to.equal('987654321');
+          expect(data.street).to.equal('456 Street');
+          expect(data.city).to.equal('Town');
+          done();
+        });
+    });
+  });
+  // ----- UC-205 Updaten van usergegevens -----
 
 //   // UC-206 Verwijderen van user
 //   describe('UC-206 Verwijderen van user', () => {
